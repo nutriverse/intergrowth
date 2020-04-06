77ce2452fd0249ff7069c8313ba1e4a787e46efb
Ernest Guevarra <ernest@validmeasures.org>
2020-04-06 07:43:47 +0100

    update package documentation

---
+++ R/data.R
@@ -0,0 +1,23 @@
+################################################################################
+#
+#' International dating standards in early pregnancy: table of centiles of
+#' gestational age in weeks and days based on crown-rump length in millimetres
+#'
+#' @format A tibble with 810 rows and 4 columns:
+#' \describe{
+#'   \item{\code{crl}}{Crown-rump length in millimetres}
+#'   \item{\code{centile}}{Centile (3rd, 10th, 50th, 90th, 97th)}
+#'   \item{\code{unit}}{Time unit for age value (week or day)}
+#'   \item{\code{age}}{Age value}
+#' }
+#'
+#' @examples
+#' gest_age_crl
+#'
+#' @source Napolitano R, Dhami J, Ohuma EO, Ioannou C, Conde-Agudelo A,
+#'     Kennedy SH et al. Pregnancy dating by fetal crown-rump length: a
+#'     systematic review of charts. BJOG 2014, 121(5):556-5.
+#'
+#
+################################################################################
+"gest_age_crl"
---
+++ R/intergrowth.R
@@ -0,0 +1,23 @@
+################################################################################
+#
+#' The INTERGROWTH-21st Project was a multi-centre, multi-ethnic,
+#' population-based project, conducted between 2009 and 2014, in eight
+#' demarcated urban areas: Pelotas, Brazil; Shunyi County, Beijing, China;
+#' Central Nagpur, India; Turin, Italy; Parklands Suburb, Nairobi, Kenya;
+#' Muscat, Oman; Oxford, UK, and Seattle, USA. Its primary aim was to study
+#' growth, health, nutrition and neurodevelopment from <14weeks of gestation to
+#' 2 years of age, using the same conceptual framework as the WHO Multicentre
+#' Growth Reference Study, so as to produce international prescriptive standards
+#' for pregnancy dating, maternal weight gain, fetal growth, newborn size, the
+#' postnatal growth of preterm infants and cognitive development at 2 years of
+#' age. These scientifically robust clinical tools may be used to monitor and
+#' evaluate maternal and fetal wellbeing, as well as infant health and nutrition
+#' at an individual and population level.
+#'
+#' @docType package
+#' @name intergrowth
+#' @keywords internal
+#'
+#
+################################################################################
+"_PACKAGE"
