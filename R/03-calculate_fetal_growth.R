7b113ca1fd8cc0dcf087cb665a591bb1f1399ab3
Ernest Guevarra <ernest@guevarra.io>
2020-04-07 20:13:53 +0100

    create new function

--- R/03-calculate_fetal_growth.R
+++ R/03-calculate_fetal_growth.R
@@ -1,11 +1,17 @@
 ################################################################################
 #
+#' Calculate head circumference for gestational age centiles and z-scores
+#'
+#' @param gestage Gestational age in exact weeks
+#' @param hc Head circumference in millimetres
+#'
+#' @return A list of 2 elements containing 1) head circumference-for-gestational
+#'   age z-score; and, 2) head circumference-for-gestational age centile.
+#'
+#' @examples
+#' calculate_hc_gestage(gestage = "90+0", hc = 78)
 #'
 #'
-#'
-#'
-#'
-#'
-#'
+#' @export
 #
 ################################################################################
